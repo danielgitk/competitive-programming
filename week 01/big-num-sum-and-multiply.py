af7409aafce532982ea8b3da9ac84c9c7c304485
Daniel <daniel@localhost.localdomain>
2021-01-01 16:48:19 +0300

    sum of negatives addd but not yet perfect

--- week 01/big-num-sum-and-multiply.py
+++ week 01/big-num-sum-and-multiply.py
@@ -7,10 +7,28 @@ Created on Thu Dec 31 09:11:41 2020
 """
 
 def sum(int1,int2):
+    diff = abs(int1) > abs(int2)
     first = str(int1)
     second = str(int2)
+    if (first[0] != '-' and (second[0] != '-')):
+        return both_positive(first, second)
+    elif (first[0] == '-' and (second[0] == '-')):
+        return '-'+ both_positive(first[1:], second[1:])
+    elif (first[0] == '-' and diff):
+        return '-'+one_negative(first[1:],second)
+    elif (second[0] == '-' and not diff):
+        return '-'+one_negative(second[1:], first)
+    elif (first[0] == '-' and not diff):
+        return one_negative(second,first[1:])
+    elif (second[0] == '-' and diff):
+        return one_negative(first,second[1:])
+    else: 
+        return '0'
+    
+    
+    
+def both_positive(first,second):
     summ = ''    
-    sign = '-' (if first[0] == '-' or second[0] == '-' ) and 
     carry = 0
     a = len(first)
     b = len(second)
@@ -18,8 +36,7 @@ def sum(int1,int2):
     if(a<b):
         first = '0'*c + first
     else:
-        second = '0'* c + second
-    
+        second = '0'* c + second    
     for i in range(a if a >= b else b):
         d = str(int(first[-i-1])+int(second[-i-1])+int(carry))
         if len(d) > 1:
@@ -31,6 +48,30 @@ def sum(int1,int2):
     summ = carry + summ   
     summ = summ[1:] if summ[0] == '0' else summ   
     return summ
+def one_negative(first,second):
+    summ = '' 
+    second = '0'*(len(first)-len(second)) + second
+    for i in range(len(first)):
+        x = int(first[-i-1])
+        y = int(second[-i-1])
+        # print(x,y)
+        if (x >=  y):            
+            diff = str(x - y)   
+            print(diff)
+        elif (x <  y ):
+            diff = str(x+10 - y)   
+            # print(diff)
+            # print(str(int(first[-i-2]) - 1))
+            # print(first)
+            x = first
+            
+            first = x[:-i-2] + str(int(x[-i-2]) - 1) + x[-i-1:]
+            print(first)
+            # print(first)
+            
+        summ = diff + summ
+    return summ
+                   
 
 def multiply(int1,int2):
     first = str(int1)
@@ -39,8 +80,7 @@ def multiply(int1,int2):
     carry = 0
     for i in range(len(second)):
         mult = ''
-        for j in range(len(first)):
-            
+        for j in range(len(first)):            
             m = str(int(first[-j-1]) * int(second[-i-1]) + int(carry))
             if len(m) >1:
                 mult = m[-1] +mult
@@ -55,7 +95,8 @@ def multiply(int1,int2):
             
              
 if __name__ == "__main__":
-    # print(sum(1234567890987654321323423541351451,12345678987654321134143124123513551134))
+    # print(sum(-1234567890987654321323423541351451,-12345678987654321134143124123513551134))
     # print(multiply(152859965,912301))
+    print(sum(1230, -123))
     
     
